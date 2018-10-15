aa1340c8a5994f7ec79b0650e34fdeebe34aa8df
Oleg Chaychenko <ochayche@e1r10p7.unit.ua>
2018-10-15 16:38:48 +0300

    Patched infix to postfix conversion to ES logic

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -2,24 +2,21 @@
   Infix to postfix conversion in C++
   Input Postfix expression must be in a desired format.
   Operands and operator, both must be single character.
-  Only '+'  ,  '-'  , '*', '/' and '^' (for exponentiation)  operators are expected.
 */
 #include<iostream>
 #include<stack>
 #include<string>
 
-using namespace std;
-
 // Function to convert Infix expression to postfix
-string InfixToPostfix(string expression);
+std::string InfixToPostfix(std::string expression);
 
 // Function to verify whether an operator has higher precedence over other
 int HasHigherPrecedence(char operator1, char operator2);
 
 // Function to verify whether a character is operator symbol or not.
-bool IsOperator(char C);
+bool IsOperatorBinary(char C);
 
-bool IsOperator2(char C);
+bool IsOperatorUnary(char C);
 
 // Function to verify whether a character is alphanumeric chanaracter
 //(letter or numeric digit) or not.
@@ -33,58 +30,59 @@ bool IsClosingParentheses(char C);
 
 int main()
 {
-	string expression;
-	cout<<"Enter Infix Expression \n";
-	getline(cin,expression);
-	string postfix = InfixToPostfix(expression);
-	cout<<"Output = "<<postfix<<"\n";
+	std::string expression;
+	std::cout<<"Enter Infix Expression \n";
+	getline(std::cin,expression);
+	std::string postfix = InfixToPostfix(expression);
+	std::cout<<"Output = "<<postfix<<"\n";
 }
 
 // Function to evaluate Postfix expression and return output
-string InfixToPostfix(string expression)
+std::string InfixToPostfix(std::string expression)
 {
 	// Declaring a Stack from Standard template library in C++.
-	stack<char> S;
-	string postfix = ""; // Initialize postfix as empty string.
-	int doubl = 0;
-	for(int i = 0;i< expression.length();i++) {
+	std::stack<char> S;
+	std::string postfix = ""; // Initialize postfix as empty std::string.
+	int unaryToBinary = 0;
+
+    S.push('('); // Push a left parenthesis ‘(‘ onto the stack.
+
+	for (int i = 0;i< expression.length(); i++) {
 
 		// Scanning each character from left.
 		// If character is a delimiter, move on.
-		if(expression[i] == ' ' || expression[i] == ',') continue;
+		if (expression[i] == ' ' || expression[i] == ',')
+			continue;
 
-		// Else if character is an operand then just append it in res string
-		else if(IsOperand(expression[i]))
+		// Else if character is an operand then just append it in res std::string
+		else if (IsOperand(expression[i]))
 		{
 			postfix +=expression[i];
-			if (doubl == 1) {
+			if (unaryToBinary == 1) {
 				postfix +=expression[i];
-				doubl = 0;
+				unaryToBinary = 0;
 			}
 		}
 
-
-		else if(IsOperator2(expression[i]))
+		else if (IsOperatorUnary(expression[i]))
 		{
-			while(!S.empty() && !IsOpeningParentheses(S.top())
-            		&& HasHigherPrecedence(S.top(),expression[i]))
+			while (!S.empty() && !IsOpeningParentheses(S.top())
+				  && HasHigherPrecedence(S.top(),expression[i]))
 			{
 				postfix+= S.top();
 				S.pop();
 			}
 			S.push(expression[i]);
-			doubl = 1;
+			unaryToBinary = 1;
 		}
-
-
 
 		//If character is operator, check for Higher precedence operator in Stack top
 		//till first opening bracket and pop all such operators
 		//Finally push the current operator on the Stack
-		else if(IsOperator(expression[i]))
+		else if (IsOperatorBinary(expression[i]))
 		{
-			while(!S.empty() && !IsOpeningParentheses(S.top())
-            		&& HasHigherPrecedence(S.top(),expression[i]))
+			while (!S.empty() && !IsOpeningParentheses(S.top())
+				   && HasHigherPrecedence(S.top(),expression[i]))
 			{
 				postfix+= S.top();
 				S.pop();
@@ -93,35 +91,57 @@ string InfixToPostfix(string expression)
 		}
 
 		//If opening Parentheses simply push it on the Stack
-		else if(IsOpeningParentheses(expression[i]))
+		else if (IsOpeningParentheses(expression[i]))
 		{
 			S.push(expression[i]);
 		}
 
-        //If Closing Parentheses then pop all the operators and append to res string
+        //If Closing Parentheses then pop all the operators and append to res std::string
         // until Stack top is opening Parentheses and finally one extra pop for Opening Parentheses
-		else if(IsClosingParentheses(expression[i]))
+		else if (IsClosingParentheses(expression[i]))
 		{
-			while(!S.empty() && !IsOpeningParentheses(S.top())) {
+			while (!S.empty() && !IsOpeningParentheses(S.top())) {
 				postfix += S.top();
 				S.pop();
 			}
+			if (S.empty()) {
+				throw std::runtime_error("missing left paren");
+			}
+			// Discard the left paren
 			S.pop();
+		}
+
+		else {
+			throw std::runtime_error("invalid input character");
 		}
 	}
 
-    //Finally pop and append all operators till Stack is not empty
-	while(!S.empty()) {
+	//Finally pop and append all operators till Stack is not empty
+
+	// Started with a left paren, now close it:
+	// While top of stack is not a left paren
+	while (!S.empty() && '(' != S.top()) {
 		postfix += S.top();
 		S.pop();
 	}
+
+    if (S.empty()) {
+        throw std::runtime_error("missing left paren");
+    }
+    // Discard the left paren
+     S.pop();
+
+     // all open parens should be closed now -> empty stack
+     if (!S.empty()) {
+         throw std::runtime_error("missing right paren");
+     }
 
 	return postfix;
 }
 
 // Function to verify whether a character is english letter or numeric digit.
 // We are assuming in this solution that operand will be a single character
-bool IsOperand(char C)
+bool IsOperand (char C)
 {
 	if(C >= '0' && C <= '9') return true;
 	if(C >= 'a' && C <= 'z') return true;
@@ -130,30 +150,28 @@ bool IsOperand(char C)
 }
 
 // Function to verify whether a character is operator symbol or not.
-bool IsOperator(char C)
+bool IsOperatorBinary(char C)
 {
-	if(C == '+' || C == '-' || C == '*' || C == '/' || C== '^')
+	if (C == '+' || C == '-' || C == '*' || C == '/' || C== '^')
 		return true;
 
 	return false;
 }
 
 // Function to verify whether a character is operator symbol or not.
-bool IsOperator2(char C)
+bool IsOperatorUnary(char C)
 {
-	if(C == '!')
+	if (C == '!')
 		return true;
 
 	return false;
 }
 
-
-
 bool IsOpeningParentheses(char C)
 {
-    if(C == '(' || C == '{' || C=='[')
-        return true ;
-    return false;
+	if(C == '(' || C == '{' || C=='[')
+		return true ;
+	return false;
 }
 
 bool IsClosingParentheses(char C)
@@ -170,6 +188,7 @@ int IsRightAssociative(char op)
 }
 
 // Function to get weight of an operator. An operator with higher weight will have higher precedence.
+#if 0
 int GetOperatorWeight(char op)
 {
 	int weight = -1;
@@ -184,11 +203,32 @@ int GetOperatorWeight(char op)
 		weight = 2;
 		break ;
 	case '^':
-	case '!':
 		weight = 3;
 	}
 	return weight;
 }
+#else /* 0 */
+int GetOperatorWeight(char op)
+{
+	int weight = -1;
+	switch(op)
+	{
+	case '^':
+		weight = 1;
+		break ;
+	case '|':
+		weight = 2;
+		break ;
+	case '+':
+		weight = 3;
+		break ;
+	case '!':
+		weight = 4;
+		break ;
+	}
+	return weight;
+}
+#endif /* 0 */
 
 // Function to perform an operation and return output.
 int HasHigherPrecedence(char op1, char op2)
@@ -201,8 +241,10 @@ int HasHigherPrecedence(char op1, char op2)
 	// if operator is left-associative, left one should be given priority.
 	if(op1Weight == op2Weight)
 	{
-		if(IsRightAssociative(op1)) return false;
-		else return true;
+		if (IsRightAssociative(op1))
+			return false;
+		else
+			return true;
 	}
 	return op1Weight > op2Weight ?  true: false;
 }
